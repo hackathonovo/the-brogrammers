3bf67adc519015aaa54f41d8c02c4015aca518a3
Domagoj Penic <dpenic23@gmail.com>
2017-05-20 17:15:40 +0200

    Created HGSS action

---
+++ HGSS-Server/app/models/HGSSAction.java
@@ -0,0 +1,24 @@
+package models;
+
+import com.avaje.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * Created by penic on 20.05.17..
+ */
+@Entity
+public class HGSSAction extends Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    public Long id;
+
+    public String description;
+
+    public static Finder<Long, HGSSAction> finder = new Finder<>(HGSSAction.class);
+
+}
