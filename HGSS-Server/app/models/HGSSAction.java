a1353b1cfedb2c184d73b101b3ca5642f5ed9948
Domagoj Penic <dpenic23@gmail.com>
2017-05-20 19:07:59 +0200

    Added geo models and dummy data, changed action

--- HGSS-Server/app/models/HGSSAction.java
+++ HGSS-Server/app/models/HGSSAction.java
@@ -1,11 +1,10 @@
 package models;
 
 import com.avaje.ebean.Model;
+import models.geo.HGSSZone;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 /**
  * Created by penic on 20.05.17..
@@ -18,6 +17,9 @@ public class HGSSAction extends Model {
     public Long id;
 
     public String description;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    public List<HGSSZone> zones;
 
     public static Finder<Long, HGSSAction> finder = new Finder<>(HGSSAction.class);
 
